11b6aace600971e77350ff888c07ee8e2bc379e1
Gareth Aguilar <garethaguilar@gmail.com>
2024-03-04 16:22:43 -0600

    Add initial files

---
+++ Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,21 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalTesting.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        static void patchUpdate(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LethalTesting
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalTestingBase : BaseUnityPlugin
+    {
+        private const string modGUID = "JiRath.LethalTesting";
+        private const string modName = "LethalTesting";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+        private static LethalTestingBase Instance;
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (!Instance)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo("Lethal Testing mod has been loaded.");
+
+            harmony.PatchAll();
+        }
+    }
+}
